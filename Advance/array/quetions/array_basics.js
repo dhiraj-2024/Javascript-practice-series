be15fbc6088bd27571ac5aae49118c2ed5d25cd6
Dhiraj Barwal <rajput.dhiraj1852@gmail.com>
2025-07-27 15:50:21 +0000

    how to create array

---
+++ Advance/array/quetions/array_basics.js
@@ -0,0 +1,57 @@
+//  ARRAY CREATION METHODS
+
+
+/*
+const cars = ["tata","bmw","mahindra","suzuki"]
+console.log(cars);
+*/
+
+
+
+// you can also create empty array and assign values later 
+
+/*
+const person = [];
+person[0] = "dhiraj";
+person[1] = "suyog";
+person[2] = "karan";
+person[3] = "pramod";
+console.log(person)
+*/
+
+
+
+// you can create array usin NEW keywords
+
+/*
+const animal = new Array("dog","cat","cow","tiger");
+console.log(animal)
+*/
+
+
+
+// ACCESS ARRAY ELEMENTS
+
+/*
+const cars = ["tata","bmw","mahindra","suzuki"]
+let tataCar = cars[0];
+console.log(tataCar);
+*/
+
+
+// CHANGING ARRAY ELEMENT 
+/*
+const cars = ["tata","bmw","mahindra","suzuki"]
+cars[1] = "toyota"
+console.log(cars); //[ 'tata', 'toyota', 'mahindra', 'suzuki' ]
+*/
+
+
+// ARRAY TO STRING CONVERSION 
+
+/*
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+document.getElementById("demo").innerHTML = fruits.toString();
+
+output: Banana,Orange,Apple,Mango
+*/
