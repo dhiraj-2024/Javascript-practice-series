7a86b6187f8d15ee58eeb706d79143d77d55b038
Dhiraj Barwal <rajput.dhiraj1852@gmail.com>
2025-07-27 09:32:40 +0000

    function definitions

--- Clear_Consepts/function/practice/function1.js
+++ Clear_Consepts/function/practice/function1.js
@@ -1,37 +1,86 @@
-//! function decleration
+//! FUNCTION DECLERATION
+/*
 function mornignMessage(){
     console.log("Good Mornign");
 }
-// mornignMessage();
-// mornignMessage();
+mornignMessage();
+mornignMessage();
+*/
 
-//! function hoisting
-// mornignMessage()
+
+//! FUNCTION HOISTING
+/*
+ mornignMessage()
 function mornignMessage(){
     console.log("Good Mornign");
 };
 
-//! function expression
+//example 2
+let x = addfun(4,5);
+console.log(x);
+function addfun(a,b){
+    return a+b;
+}
+
+*/
+
+//! FUNCTION EXPRESSION
+// A JavaScript function can also be defined using an expression.
+// A function expression can be stored in a variable:
+
+/*
 const sayName = function(){
     console.log("dhiraj");
 }
-// sayName();
+sayName();
 
-//! Arrow function
+*/
+
+
+//! ARROW FUNCTION
+/*
 const sayHello = ()=>{
     console.log("hello")
 }
-// sayHello()
+sayHello()
+*/
 
-//! one line arrow function
+//! ONE LINE ARROW FUNCTION
+/*
 const addsum = (a,b)=> a+b ;
-// console.log(addsum(2,5))
+console.log(addsum(2,5))
+*/
 
-//!anonynous function
-// setTimeout(function () {
-//     console.log("This is an anonymous function");
-//   }, 1000);
+
+//!ANONYOMOUS FUNCTION
+/*
+setTimeout(function () {
+    console.log("This is an anonymous function");
+  }, 1000);
+*/
   
+
+//! FUNCTION CUNSTRUCTOR
+/*
+const myFunction = new Function("a","b","return a * b")
+let x = myFunction(3,5)
+console.log(x);
+*/
+/*
+const myFunction = function (a,b) {return a * b }
+let x = myFunction(3,5)
+console.log(x);
+*/
+
+//! SELF INVOKING FUNCTION 
+/*
+(function greet(){
+    console.log("good afternoon")
+})()
+*/
+
+
+
 
 function outer() {
     return function () {
