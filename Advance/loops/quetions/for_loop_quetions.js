7978ab06f1ee46a9ce5f95bf0ee36bbee986248d
Dhiraj Barwal <rajput.dhiraj1852@gmail.com>
2025-07-28 14:58:23 +0000

    loops quetion

--- Advance/loops/quetions/for_loop_quetions.js
+++ Advance/loops/quetions/for_loop_quetions.js
@@ -4,7 +4,15 @@ let sum = 0;
 for( let i=0; i<=5; i++){
  sum = sum + i;
 }
-console.log(sum); // 15
+console.log("sum :",sum); // 15
 
 // 2. Print all even numbers from 0-100
 
+for (let i=0; i<=100; i++){
+if(i%2==0){
+    console.log("even numbers :",i)
+}
+// else if(i%2 !== 0){
+//     console.log("odd numbers :",i)
+// }
+}
