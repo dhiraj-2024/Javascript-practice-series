e3d1001446e64b7ca45842656001631c0c8a4c4a
Dhiraj Barwal <rajput.dhiraj1852@gmail.com>
2025-07-29 12:48:29 +0000

    date create and type

---
+++ Advance/date/dates.js
@@ -0,0 +1,28 @@
+// Date Object 
+// There are 9 ways to create a new date object:
+/*
+new Date()
+new Date(date string)
+new Date(year,month)
+new Date(year,month,day)
+new Date(year,month,day,hours)
+new Date(year,month,day,hours,minutes)
+new Date(year,month,day,hours,minutes,seconds)
+new Date(year,month,day,hours,minutes,seconds,ms)
+new Date(milliseconds)
+*/
+
+const d = new Date()
+console.log(d)
+
+const d1 = new Date("2025-07-29")
+console.log(d1)
+
+// Js Date Format 
+/*
+Type	     Example
+ISO Date	"2015-03-25" (The International Standard)
+Short Date	"03/25/2015"
+Long Date	"Mar 25 2015" or "25 Mar 2015"
+*/
+
