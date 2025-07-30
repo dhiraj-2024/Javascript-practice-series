de6db9cd0b3fd0e4eacdc1aa478819dbaf24b8bf
Dhiraj Barwal <rajput.dhiraj1852@gmail.com>
2025-07-30 16:13:42 +0000

    assignment operators

---
+++ Advance/operators/assignment_operators.js
@@ -0,0 +1,35 @@
+// Assignment Operators 
+
+// = 
+// - used to assign a value to variable
+
+let a = 10;
+console.log("value of a is :",a); //10
+
+
+// compound operator
+
+// += add then assin
+ a += 10;
+ console.log("addition : ",a) // addition :  20
+
+// += subtract then assin
+ a -= 10;
+ console.log("subtraction : ",a) //subtraction :  10
+
+// += multipy then assin
+ a *= 10;
+ console.log("multiplication : ",a) // multiplication :  100
+
+// += divide then assin
+  a /= 10;
+ console.log("division : ",a) // division :  10
+
+// += modulus then assin
+  a %= 8;
+ console.log("reminder/module : ",a) // reminder/module :  2
+
+// += power then assin
+  a **= 2;
+ console.log("power : ",a) // power :  4
+
