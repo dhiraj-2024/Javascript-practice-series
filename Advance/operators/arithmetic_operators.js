b27c96d04cd0fe892cefa93d3378cc65b161abe4
Dhiraj Barwal <rajput.dhiraj1852@gmail.com>
2025-07-30 15:56:59 +0000

    arithmetic operations with operator

--- Advance/operators/arithmetic_operators.js
+++ Advance/operators/arithmetic_operators.js
@@ -1,4 +1,14 @@
 // Arithmetic Operators 
-/*
-- used to perform arithmetic operation on numbers
-*/
+
+let a = 10;
+let b = 2;
+
+console.log("addition is :",a+b);  
+console.log("substraction is : ",a-b);
+console.log("multiplication  is :",a*b);
+console.log("division is :",a/b);
+console.log("reminder is :",a%b);
+console.log("exponential is :",a**b);
+console.log("post increment is :",a++);
+console.log("post decrement is :",a--);
+
