06f764318422c9820a38fa6a014ed46ec4513958
Dhiraj Barwal <rajput.dhiraj1852@gmail.com>
2025-08-05 13:09:02 +0000

    vowels and consonents count from string

--- Advance/string/problems/vowels-consonents.js
+++ Advance/string/problems/vowels-consonents.js
@@ -1,2 +1,39 @@
 // Count vowels and consonants in a string
 
+// Method 1:
+
+let checkVowelsAndConsonents =(str)=>{
+    let lowerString = str.toLowerCase();
+    let vCount = 0;
+    let cCount = 0;
+    const vowels = "aeiou";
+    for(let char of lowerString){
+      if(/[a-z]/.test(char)){
+        if(vowels.includes(char)){
+            vCount++;
+        }else{
+            cCount++;
+        }
+      }
+    }
+  console.log(`Vowels are : ${vCount} and Consonents are: ${cCount}`)
+}
+
+console.log(checkVowelsAndConsonents("dhirajiii"))
+
+
+// Method 2:
+
+const checkVandC =(str)=>{
+    const vowels = new Set(['a','e','i','o','u']);
+    let vowelC = 0;
+    let consonentC = 0;
+    [...str.toLowerCase()].forEach((char)=>{
+        if(/[a-z]/.test(char)){
+            vowels.has(char) ? vowelC++ : consonentC++;
+        }
+    });
+    console.log(`vowels are: ${vowelC} and consonents are: ${consonentC}`)
+}
+
+checkVandC("hello")
