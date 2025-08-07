d71337e32627dca7267acf3b36c1f5cef3182f30
Dhiraj Barwal <rajput.dhiraj1852@gmail.com>
2025-08-07 17:52:07 +0000

    Capitalize the first letter of each word

--- Advance/string/problems/capitalized_first_letter.js
+++ Advance/string/problems/capitalized_first_letter.js
@@ -1,13 +1,22 @@
 // Capitalize the first letter of each word
 
-const capiFirstLetter = (str)=>{
-     let string = str.split("");
-     let result = [];
-     for (let words of string){
-        const capitalized = words.charAt(0).toUpperCase() + words.slice(1);
-        result.push[capitalized]
-     }
-    let result.join("")
-}
+const capiFirstLetter = (str) => {
+  let words = str.split(" ");
+  let result = [];
 
-console.log(capiFirstLetter("hello how are you"))
+  for (let word of words) {
+    const capitalized = word.charAt(0).toUpperCase() + word.slice(1);
+    result.push(capitalized);
+  }
+
+  return result.join(" ");
+};
+
+console.log(capiFirstLetter("hello how are you"));
+// Output: "Hello How Are You"
+
+
+
+
+const capiFirstLetters = str =>
+  str.split(" ").map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(" ");
